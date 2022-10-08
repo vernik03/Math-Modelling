3bbeba593a8cf43b4fc7f34f5fc9e5fa4178a5f2
vernik03 <vernik03@gmail.com>
2022-10-08 20:47:35 +0300

    DFT

--- Fourier.py
+++ Fourier.py
@@ -1,1 +1,14 @@
+from ast import main
 import numpy as np
+
+# дискретное преобразование Фурье
+def DFT(x):
+    N = len(x)
+    n = np.arange(N)
+    k = n.reshape((N, 1))
+    M = np.exp(-2j * np.pi * k * n / N)
+    return np.dot(M, x)
+
+if __name__ == '__main__':
+    x = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+    print(DFT(x))
